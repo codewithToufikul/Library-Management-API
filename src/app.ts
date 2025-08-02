53f8cb3d2c782dd683fc16ef528992cd16b702bd
codewithToufikul <imranmeg2356@gmail.com>
2025-08-03 01:14:49 +0600

    book borrow route added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import dotenv from "dotenv";
 import cors from "cors";
 import { bookRoute } from './app/controllers/book.controller';
+import { borrowRoute } from './app/controllers/borrow.controller';
 
 
 const app: Application = express();
@@ -10,6 +11,7 @@ app.use(cors());
 dotenv.config();
 
 app.use("/api/books", bookRoute)
+app.use("/api/borrow", borrowRoute)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Api is running');
---
+++ src/app/controllers/borrow.controller.ts
@@ -0,0 +1,45 @@
+import express, { Request, Response } from "express";
+import { Book } from "../models/book.model";
+import { Borrow } from "../models/borrow.model";
+
+export const borrowRoute = express.Router();
+
+// borrow a book
+borrowRoute.post("/", async (req: Request, res: Response) => {
+  try {
+    const { book: bookId, quantity, dueDate } = req.body;
+
+    const book = await Book.findById(bookId);
+    if (!book) {
+      return res.status(404).json({
+        success: false,
+        message: "Book not found",
+      });
+    }
+    if (book.copies < quantity) {
+      return res.status(400).json({
+        success: false,
+        message: `Only ${book.copies} copies are available`,
+      });
+    }
+    book.copies -= quantity;
+    await book.save();
+    await Book.updateAvailability(bookId);
+    const borrow = await Borrow.create({
+      book: book._id,
+      quantity,
+      dueDate,
+    });
+    res.status(201).json({
+      success: true,
+      message: "Book borrowed successfully",
+      data: borrow,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message || "Failed to Borrow book",
+      error,
+    });
+  }
+});
