b3dbf96ed125d365b82c182b63ec5e2440be287a
codewithToufikul <imranmeg2356@gmail.com>
2025-08-03 11:52:03 +0600

    Borrowed Books Summary (Using Aggregation)

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -43,3 +43,50 @@ borrowRoute.post("/", async (req: Request, res: Response) => {
     });
   }
 });
+
+// get borrow with agregation
+
+borrowRoute.get("/", async (req: Request, res: Response) => {
+  try {
+    const summary = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
+      },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookDetails",
+        },
+      },
+      {
+        $unwind: "$bookDetails",
+      },
+      {
+        $project: {
+          _id: 0,
+          book: {
+            title: "$bookDetails.title",
+            isbn: "$bookDetails.isbn",
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
+      data: summary,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message || "Failed to Fetch Borrow book",
+      error,
+    });
+  }
+});
